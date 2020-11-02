1d6f104a7bb1667a351a0748ecc9af073200de8a
root <root@Jonah.localdomain>
2020-11-02 15:39:09 +0100

    funkar typ

--- index.js
+++ index.js
@@ -1,18 +1,16 @@
 function getUserGuess(){
-    document.getElementById("message").innerHTML = "Nu har du gissat!"
+    document.getElementById("message").innerHTML = "Du gissade inte rätt. Tips tal nära 70"
    const input = document.getElementById("user-input").value;
-if (getUserGuess === "69"){
-    alert("Du har gissat rätt!")
+   return parseInt(input)
 }
-return parseInt(input)
-}
-
 document.addEventListener("keyup", function (event) {
 if (event.key === "Enter") {
     const svar = getUserGuess()
     console.log(svar)
+if (svar === 69)
+document.getElementById("message").innerHTML = "Du har gissat Rätt"
 }
 else {
-    document.getElementById("message").innerHTML = "Du har inte gissat"
+    document.getElementById("message").innerHTML = "Du har inte gissat än"
 }
 })
