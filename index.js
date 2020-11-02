8df224a5267fa389624b09ca37419e16f37ceecb
root <root@Jonah.localdomain>
2020-11-02 15:44:55 +0100

    FUNKAR!

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 function getUserGuess(){
-    document.getElementById("message").innerHTML = "Du gissade inte rätt. Tips tal nära 70"
    const input = document.getElementById("user-input").value;
    return parseInt(input)
 }
@@ -8,7 +7,13 @@ if (event.key === "Enter") {
     const svar = getUserGuess()
     console.log(svar)
 if (svar === 69)
-document.getElementById("message").innerHTML = "Du har gissat Rätt"
+document.getElementById("message").innerHTML = "Du har gissat Rätt!!!"
+if (svar === 96)
+document.getElementById("message").innerHTML = "Extremt Nära!"
+if (svar > 69)
+document.getElementById("message").innerHTML = "Mindre!"
+if (svar < 69)
+document.getElementById("message").innerHTML = "Större!"
 }
 else {
     document.getElementById("message").innerHTML = "Du har inte gissat än"
