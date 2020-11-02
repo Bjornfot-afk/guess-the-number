2e172508e36c2015cf29ede2a38efe92a938b2aa
root <root@Jonah.localdomain>
2020-11-02 15:15:44 +0100

    lite längr

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
-var hej = Math.round(Math.random()* 100) 
-console.log(hej)
+function getUserGuess(){
+    document.getElementById("message").innerHTML = "Nu har du gissat!"
+   const input = document.getElementById("user-input").value;
+   return parseInt(input)
+}
+
+document.addEventListener("keyup", function (event) {
+if (event.key === "Enter") {
+    const svar = getUserGuess()
+    console.log(svar)
+}
+else {
+    document.getElementById("message").innerHTML = "Du har inte gissat"
+}
+})
